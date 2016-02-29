612549a8d0a22ff6d0e3739582a94a91a3a12248
Henry Aguinaga <aguinagahenry@gmail.com>
2016-02-29 12:11:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/henry/listview3/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.henry.listview3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+    String[] elementos = {"Jose","Pedro","Maria","Miguel", "Luis","Helena", "Laura", "Linda","Alice","Adrian"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        listView = (ListView) findViewById(R.id.listview);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,elementos);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(getApplicationContext(),String.valueOf(position),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
